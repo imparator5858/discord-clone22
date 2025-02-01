30e9c458901c69241b9b68eaa074d439401dd975
imparator5858 <150621938+imparator5858@users.noreply.github.com>
2025-02-01 23:16:54 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 const config = {};
 
 config.port = process.env.PORT || 5000;
-config.dbURL = process.env.DATABASEURL || "mongodb+srv://imparator:NXMmr0PnTSyGTPBC@xeox.t5rfqye.mongodb.net/";
-
+config.dbURL = process.env.DATABASEURL || "mongodb+srv://imparator:NXMmr0PnTSyGTPBC>@xeox.t5rfqye.mongodb.net/";
 module.exports = config;
